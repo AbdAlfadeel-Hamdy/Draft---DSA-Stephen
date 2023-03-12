af7e3efcacbbc56471014c418ad762bcd50c8079
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-03-12 18:11:21 +0200

    max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  for (const char of str.split('')) {
+    // if (charMap[char]) charMap[char]++;
+    // else charMap[char] = 1;
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  let maxChar;
+  for (const char in charMap) {
+    if (charMap[char] > charMap[maxChar] || !maxChar) maxChar = char;
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
