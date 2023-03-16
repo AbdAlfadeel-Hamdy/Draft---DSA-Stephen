5721d84b625c69e819ad27611fc06c2534e36ae9
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-03-16 20:12:17 +0200

    solve steps problem (1)

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,22 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Solution (1)
+// function steps(n) {
+//   for (let i = 1; i <= n; i++)
+//     console.log(`${'#'.repeat(i)}${' '.repeat(n - i)}`);
+// }
+
+// Solution (2)
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+    for (let col = 0; col < n; col++) {
+      if (col <= row) stair += '#';
+      else stair += ' ';
+    }
+    console.log(stair);
+  }
+}
 
 module.exports = steps;
