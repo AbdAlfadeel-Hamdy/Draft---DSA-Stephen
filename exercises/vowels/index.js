b6789ef0aba55003f76664c8675e3a48b5044458
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-03-23 19:25:22 +0200

    solve vowels problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// Solution (1)
+// function vowels(str) {
+//   const vowels = ['a', 'e', 'i', 'o', 'u'];
+//   let vowelsCount = 0;
+//   for (const char of str.toLowerCase())
+//     if (vowels.includes(char)) vowelsCount++;
+//   return vowelsCount;
+// }
+
+// Solution (2)
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
