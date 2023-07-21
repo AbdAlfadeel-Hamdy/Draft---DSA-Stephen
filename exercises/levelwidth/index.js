3cf0a3c2651aaef710066df9c56d615c5db07b36
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-07-21 06:46:05 +0300

    levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,39 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  // Solution (1)
+  // if (!root) return [];
+  // const result = [1];
+  // const arr = [[root]];
+  // while (arr.length) {
+  //   const removedNode = arr.pop();
+  //   const newEl = [];
+  //   removedNode.forEach((el) => {
+  //     for (let i = 0; i < el.children.length; i++) {
+  //       newEl.push(el.children[i]);
+  //     }
+  //   });
+  //   if (!newEl.length) return result;
+  //   result.push(newEl.length);
+  //   arr.push(newEl);
+  // }
+  // return result;
+  // Solution (2)
+  if (!root) return [];
+  const result = [0];
+  const arr = [root, 'S'];
+  while (arr.length > 1) {
+    const removedNode = arr.shift();
+    if (removedNode === 'S') {
+      result.push(0);
+      arr.push('S');
+    } else {
+      result[result.length - 1] = result[result.length - 1] + 1;
+      arr.push(...removedNode.children);
+    }
+  }
+  return result;
+}
 
 module.exports = levelWidth;
