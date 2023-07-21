bfce9b50ecbcfd7520599de44e2ab0da66b71696
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-07-21 19:55:26 +0300

    bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,43 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+  insert(value) {
+    // Solution (1)
+    // const newNode = new Node(value);
+    // let currentNode = this;
+    // if (value > currentNode.data) {
+    //   if (!currentNode.right) return (currentNode.right = newNode);
+
+    //   currentNode = currentNode.right;
+    //   return currentNode.insert(value);
+    // }
+    // if (value < currentNode.data) {
+    //   if (!currentNode.left) return (currentNode.left = newNode);
+
+    //   currentNode = currentNode.left;
+    //   return currentNode.insert(value);
+    // }
+
+    // Solution (2)
+    if (value > this.data && this.right) return this.right.insert(value);
+    if (value > this.data && !this.right) return (this.right = new Node(value));
+    if (value < this.data && this.left) return this.left.insert(value);
+    if (value < this.data && !this.left) return (this.left = new Node(value));
+  }
+
+  contains(data) {
+    if (this.data === data) return this;
+    if (data > this.data && this.right) return this.right.contains(data);
+    if (data > this.data && !this.right) return null;
+    if (data < this.data && this.left) return this.left.contains(data);
+    if (data < this.data && !this.left) return null;
+  }
+}
 
 module.exports = Node;
