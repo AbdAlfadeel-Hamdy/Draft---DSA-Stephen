3677ba8fc27274aad9deac77d4dad46912450ae4
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-07-27 00:12:18 +0300

    events

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,22 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  if (node.left) {
+    if (node.data > node.left.data) return validate(node.left, null, node);
+    return false;
+  }
+  if (node.right) {
+    if (
+      node.data < node.right.data &&
+      node.right.data > min &&
+      node.right.data < max
+    )
+      return validate(node.right, node.right, node);
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = validate;
